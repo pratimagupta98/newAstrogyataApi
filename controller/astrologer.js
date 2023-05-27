8c4641798701f9420b5e36a56575ffd5849c2500
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2023-05-27 13:31:12 +0530

    comit

--- controller/astrologer.js
+++ controller/astrologer.js
@@ -181,12 +181,15 @@ exports.astrosignup = async (req, res) => {
   }
 }
 exports.editAstroDetails = async (req, res) => {
-  const { fullname, email, password, cnfmPassword, gender, dob, primary_skills, all_skills, language, exp_in_years, conrubute_hrs, hear_abt_astrology, other_online_platform, why_onboard_you, suitable_tym_interview, crnt_city, income_src, highest_qualification, degree_deploma, clg_scl_name, lrn_abt_astrology, insta_link, fb_link, linkedln_link, youtube_link, website_link, anybody_prefer, min_earning_expe, max_earning_expe, long_bio, status, callCharge, min_amount, max_amount, monday, tuesday, wednesday, thursday, friday, saturday, sunday, channelName } = req.body;
+  const { mobile, fullname, email, password, cnfmPassword, gender, dob, primary_skills, all_skills, language, exp_in_years, conrubute_hrs, hear_abt_astrology, other_online_platform, why_onboard_you, suitable_tym_interview, crnt_city, income_src, highest_qualification, degree_deploma, clg_scl_name, lrn_abt_astrology, insta_link, fb_link, linkedln_link, youtube_link, website_link, anybody_prefer, min_earning_expe, max_earning_expe, long_bio, status, callCharge, min_amount, max_amount, monday, tuesday, wednesday, thursday, friday, saturday, sunday, channelName } = req.body;
 
 
 
   let data = {};
 
+  if (mobile) {
+    data.mobile = mobile
+  }
   if (fullname) {
     data.fullname = fullname
   }
