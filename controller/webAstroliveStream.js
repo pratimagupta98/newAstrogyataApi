cf49f0328890a6bc9cb71e0852d0b842e83fd732
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2023-06-21 16:31:52 +0530

    commit

--- controller/webAstroliveStream.js
+++ controller/webAstroliveStream.js
@@ -18,7 +18,7 @@ exports.WebliveStream = async (req, res) => {
 };
 
 exports.closeLiveStream = async (req, res) => {
-    const getId = await AsLive.deleteOne({ _id: req.params.id })
+    const getId = await WebLivestream.deleteOne({ _id: req.params.id })
 
         .then((data) => resp.deleter(res, data))
         .catch((error) => resp.errorr(res, error));
