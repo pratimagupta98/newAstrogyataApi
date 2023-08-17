506a23769fa9dcfe4519be15641eea90f9425794
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2023-08-17 18:13:18 +0530

    COMMIT ask Qusetion

--- controller/ASK_qus.js
+++ controller/ASK_qus.js
@@ -72,79 +72,79 @@ const Astroproduct = require("../models/astroproduct");
 exports.user_ask_qus = async (req, res) => {
   const { astroid, userid, question, answer, bundleOffer } = req.body;
 
-  // try {
-  const getdata = await Order.find({
-    $and: [
-      { status: "COMPLETED" },
-      { astroid: req.body.astroid },
-      { userid: userid },
-    ],
-  })
-    .populate({
-      path: "products",
-      populate: {
-        path: "product",
-      },
-    });
-  console.log("getdata", getdata)
-  if (getdata.length === 0) {
-    // No data found for the given criteria
-    return res.status(404).json({ msg: "No data found for the given criteria" });
-  }
+  try {
+    const getdata = await Order.find({
+      $and: [
+        { status: "COMPLETED" },
+        { astroid: req.body.astroid },
+        { userid: userid },
+      ],
+    })
+      .populate({
+        path: "products",
+        populate: {
+          path: "product",
+        },
+      });
+    console.log("getdata", getdata)
+    if (getdata.length === 0) {
+      // No data found for the given criteria
+      return res.status(404).json({ msg: "No data found for the given criteria" });
+    }
 
-  console.log("getdatassss", getdata);
-  let totalQsCount = 0;
-  for (const order of getdata) {
-    totalQsCount += order.products.qsCount;
-  }
-  const questions = await Askqustion.find({ userid: userid });
-  console.log("totalQsCount", totalQsCount - questions.length);
-  let remainqus = totalQsCount - questions.length;
-  const getete = await Askqustion.findOneAndUpdate(
-    { astroid: astroid, userid: userid },
-    { $set: { remaining_qus: totalQsCount - questions.length } },
-    { new: true }
-  );
-  console.log("getete", getete);
-  console.log("remainqus", remainqus);
-  if (questions.length > totalQsCount - 1) {
-    console.log("your question limit is over");
-    res.status(403).json({
-      msg: "Your question limit is over",
-      limit: totalQsCount,
-    });
-
-    const lastQuestion = questions[questions.length - 1];
-
-    await Askqustion.findOneAndUpdate(
-      { _id: lastQuestion._id },
-      { $set: { view_button: false, remaining_qus: remainqus } },
+    console.log("getdatassss", getdata);
+    let totalQsCount = 0;
+    for (const order of getdata) {
+      totalQsCount += order.products.qsCount;
+    }
+    const questions = await Askqustion.find({ userid: userid });
+    console.log("totalQsCount", totalQsCount - questions.length);
+    let remainqus = totalQsCount - questions.length;
+    const getete = await Askqustion.findOneAndUpdate(
+      { astroid: astroid, userid: userid },
+      { $set: { remaining_qus: totalQsCount - questions.length } },
       { new: true }
     );
-  } else {
-    const newAskqustion = new Askqustion({
-      astroid: astroid,
-      userid: userid,
-      question: question,
-      answer: answer,
-      bundleOffer: bundleOffer,
-      remaining_qus: remainqus - 1,
-      totalQus: totalQsCount,
-    });
+    console.log("getete", getete);
+    console.log("remainqus", remainqus);
+    if (questions.length > totalQsCount - 1) {
+      console.log("your question limit is over");
+      res.status(403).json({
+        msg: "Your question limit is over",
+        limit: totalQsCount,
+      });
 
-    newAskqustion
-      .save()
-      .then((data) => resp.successr(res, data))
-      .catch((error) => resp.errorr(res, error));
+      const lastQuestion = questions[questions.length - 1];
+
+      await Askqustion.findOneAndUpdate(
+        { _id: lastQuestion._id },
+        { $set: { view_button: false, remaining_qus: remainqus } },
+        { new: true }
+      );
+    } else {
+      const newAskqustion = new Askqustion({
+        astroid: astroid,
+        userid: userid,
+        question: question,
+        answer: answer,
+        bundleOffer: bundleOffer,
+        remaining_qus: remainqus - 1,
+        totalQus: totalQsCount,
+      });
+
+      newAskqustion
+        .save()
+        .then((data) => resp.successr(res, data))
+        .catch((error) => resp.errorr(res, error));
+    }
+  } catch (error) {
+    if (error.name === 'StrictPopulateError') {
+      // Handle populate error and send a user-friendly response
+      return res.status(400).json({ msg: "Order Not Found" });
+    }
+    console.error("Error in user_ask_qus:", error);
+    resp.errorr(res, error);
   }
-  // } catch (error) {
-  //   if (error.name === 'StrictPopulateError') {
-  //     // Handle populate error and send a user-friendly response
-  //     return res.status(400).json({ msg: "Order Not Found"});
-  //   }
-  //   console.error("Error in user_ask_qus:", error);
-  //   resp.errorr(res, error);
-  // }
 };
 
 
