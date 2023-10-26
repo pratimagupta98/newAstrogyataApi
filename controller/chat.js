8a75caefb61df0cb5da6566cdc734da7e543a52f
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2023-10-26 16:23:18 +0530

    chat api changes

--- controller/chat.js
+++ controller/chat.js
@@ -5,11 +5,13 @@ const Chatroom = require("../models/chatroom");
 const resp = require("../helpers/apiResponse");
 const { v4: uuidv4 } = require("uuid");
 var cron = require('node-cron');
-
+const CircularJSON = require("circular-json");
+const axios = require("axios");
 // cron.schedule('*/10 * * * * *', () => {
 //   console.log('running a task every minute');
 // });
 
+let callDetails = { sid: "", userId: "" };
 
 exports.addchat = async (req, res) => {
   const uniqueroom = uuidv4();
@@ -32,8 +34,10 @@ exports.addchat = async (req, res) => {
     new_unread_msg: 1,
     // roomid:  
   });
-  const findchatroom = await Chatroom.findOne({ $and: [{ userid: req.params.id }, { astroid: astroid }] });
-
+  const findchatroom = await Chatroom.findOne({ $and: [{ userid: req.params.id }, { astroid: astroid }] }).populate("astroid").populate("userid")
+  console.log(findchatroom)
+  let getusermo = findchatroom.userid?.mobile
+  let getastromo = findchatroom.astroid?.mobile
   // console.log("FINDDATA",findchatroom)
   if (findchatroom) {
     newChat.roomid = findchatroom._id;
@@ -57,6 +61,51 @@ exports.addchat = async (req, res) => {
       { new: true }
     );
     // console.log("updatechat", updatechat)
+    const key = "d909e2e0120d0bcbd2ef795dd19eb2e97c2f8d78d8ebb6d4";
+    const sid = "sveltosetechnologies2";
+    const token = "856371fe6a97e8be8fed6ab14c95b4832f82d1d3116cb17e";
+    const from = req.body.from;
+    const to = req.body.to;
+    const formUrlEncoded = (x) =>
+      Object.keys(x).reduce(
+        (p, c) => p + `&${c}=${encodeURIComponent(x[c])}`,
+        ""
+      );
+    const url = `https://${key}:${token}@api.exotel.in/v1/Accounts/${sid}/Calls/connect.json`;
+    const response = await axios.post(
+      url,
+      formUrlEncoded({
+        From: getusermo, //USER
+        To: getastromo, //ASTRO
+        // userid: req.body.userid,
+        // astroid: req.body.astroid.trim(), // Remove extra whitespace using trim()
+        walletId: req.body.walletId,
+        CallerId: "080-473-59942",
+        CallerType: "promo",
+        // TimeLimit: parseInt(user.amount / astrologer.callCharge) * 60,
+      }),
+      {
+        withCredentials: true,
+        headers: {
+          Accept: "application/x-www-form-urlencoded",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    const str = CircularJSON.stringify(response.data);
+    console.log("@@@@@@", response)
+    const getdata = JSON.parse(str);
+    console.log(getdata);
+    callDetails.sid = getdata.Call?.Sid;
+    let options = {
+      From: req.body.From, //USER\
+      To: req.body.To, //ASTRO
+      //userid: req.body.userid,
+      //astroid: req.body?.astroid,
+      Sid: getdata.Call?.Sid,
+
+    };
     newChat
       .save()
       .then((data) => resp.successr(res, data))
